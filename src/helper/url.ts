6f524d2ac9f6a1196d6185f836fb1af5fa766248
王圣玉 <wangshengyu@huohua.cn>
2020-01-31 17:54:56 +0800

    添加buildurl工具函数

---
+++ src/helper/url.ts
@@ -0,0 +1,47 @@
+import { isDate, isObject } from './utils'
+
+function encode(value: string): string {
+  return encodeURIComponent(value)
+    .replace(/%40/g, '@')
+    .replace(/%2A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+
+    let values = []
+
+    if (Array.isArray(val)) {
+      values = val
+
+      key += '[]'
+    } else {
+      values = [val]
+    }
+
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+}
